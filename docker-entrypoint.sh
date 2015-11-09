8acfcb6c78ce9672d05c69f04c049551a026921a
Shawn Ma <qsma@thoughtworks.com>
2015-11-09 15:16:05 +0800

    update

---
+++ docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#enable job control in script
+set -e -m
+
+#####   variables  #####
+
+# add command if needed
+if [ "${1:0:1}" = '-' ]; then
+  set -- java "$@"
+fi
+
+#run command in background
+if [ "$1" = 'java' ]; then
+  ##### pre scripts  #####
+  echo "========================================================================"
+  echo "initialize:"
+  echo "========================================================================"
+  mkdir -p "$JENKINS_HOME" && chown -R jenkins:jenkins "$JENKINS_HOME"
+  
+  ##### run scripts  #####
+  echo "========================================================================"
+  echo "startup:"
+  echo "========================================================================"
+  exec gosu jenkins "$@" &
+
+  ##### post scripts #####
+  echo "========================================================================"
+  echo "configure:"
+  echo "========================================================================"
+  
+  #bring command to foreground
+  fg
+else
+  exec "$@"
+fi
