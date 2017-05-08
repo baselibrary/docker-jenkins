f17c908d96a377fb391a9b160049f4bf823740b6
Shawn Ma <qsma@thoughtworks.com>
2017-05-08 18:58:24 +0800

    update

--- conf/init.groovy.d/20-security-ldap.groovy
+++ conf/init.groovy.d/20-security-ldap.groovy
@@ -2,8 +2,47 @@ import jenkins.model.*
 import hudson.util.*
 import hudson.security.*
 import groovy.json.JsonSlurper
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.Cipher
+
+// Helper Class
+class Crypter {
+  // Key must be exactly 16 bytes
+  def expandKey (def secret) {
+    for (def i=0; i<4; i++) {
+      secret += secret
+    }
+    return secret.substring(0, 16)
+  }
+
+  // do the magic
+  def encrypt (def plainText, def secret) {
+    secret = expandKey(secret)
+    def cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    SecretKeySpec key = new SecretKeySpec(secret.getBytes("UTF-8"), "AES")
+    cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(secret.getBytes("UTF-8")))
+
+    return cipher.doFinal(plainText.getBytes("UTF-8")).encodeBase64().toString()
+  }
+
+  // undo the magic
+  def decrypt (def cypherText, def secret) {
+    byte[] decodedBytes = cypherText.decodeBase64()
+
+    secret = expandKey(secret)
+    def cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    SecretKeySpec key = new SecretKeySpec(secret.getBytes("UTF-8"), "AES")
+    cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(secret.getBytes("UTF-8")))
+
+    return new String(cipher.doFinal(decodedBytes), "UTF-8")
+  }
+}
+
+//############### Main ###################//
 
 Jenkins instance = Jenkins.getInstance()
+def crypter = new Crypter()
 def file = new File("/usr/share/jenkins/rancher/jenkins.json")
 
 if ( instance.pluginManager.activePlugins.find { it.shortName == "ldap" } != null && file.exists()) {
@@ -20,7 +59,7 @@ if ( instance.pluginManager.activePlugins.find { it.shortName == "ldap" } != nul
           groupSearchFilter = state.security.ldap.groupSearchFilter,
           groupMembershipFilter = null,
           managerDN = state.security.ldap.managerDN,
-          managerPassword = new String(state.security.ldap.managerPassword.decodeBase64()),
+          managerPassword = crypter.decrypt(state.security.ldap.managerPassword, "ThoughtWorks"),
           inhibitInferRootDN = false,
           disableMailAddressResolver = false,
           cache = null)
