3318c4db759c520053a64496353559cd0f96a689
Shawn Ma <qsma@thoughtworks.com>
2017-05-10 14:22:09 +0800

    update

--- conf/init.groovy.d/10-theme.groovy
+++ conf/init.groovy.d/10-theme.groovy
@@ -4,6 +4,6 @@ Jenkins instance = Jenkins.getInstance()
 
 if ( instance.pluginManager.activePlugins.find { it.shortName == "simple-theme-plugin" } != null ){
   def theme = instance.getExtensionList("org.codefirst.SimpleThemeDecorator")[0]
-  theme.cssUrl = 'https://cdn.rawgit.com/afonsof/jenkins-material-theme/gh-pages/dist/material-light-blue.css'
+  theme.cssUrl = 'jenkins.css'
   theme.save()
 }
